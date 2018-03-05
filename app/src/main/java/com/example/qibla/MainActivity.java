08ef20bedc0bb1316d4e05e5dffded52d868f71b
fatimamohamed <adropofilm@gmail.com>
2018-03-04 21:11:32 -0600

    Textview animation

--- app/src/main/java/com/example/qibla/MainActivity.java
+++ app/src/main/java/com/example/qibla/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.qibla;
 
+import android.animation.AnimatorSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.animation.ObjectAnimator;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView qiblaName;
+    long animationDuration = 1000; //miliseconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        qiblaName.findViewById(R.id.qibla_name);
     }
+/*
+    public void slideTextLeft(View view) {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(qiblaName, "x", 420f);
+        animator.setDuration(animationDuration);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(animator);
+        animatorSet.start();
+    }
+    */
+
+
 }
