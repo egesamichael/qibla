c04a0078c5cbeb8b5688bfa46305c607ca8948de
fatimamohamed <adropofilm@gmail.com>
2018-03-19 22:55:57 -0500

    Sprint #2 UI added for Main Activity.

--- app/src/main/java/com/example/qibla/MainActivity.java
+++ app/src/main/java/com/example/qibla/MainActivity.java
@@ -1,31 +1,35 @@
 package com.example.qibla;
 
 import android.animation.AnimatorSet;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.animation.ObjectAnimator;
 
 public class MainActivity extends AppCompatActivity {
     private TextView qiblaName;
-    long animationDuration = 1000; //miliseconds
+    private Button initiate_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        qiblaName.findViewById(R.id.qibla_name);
-    }
-/*
-    public void slideTextLeft(View view) {
-        ObjectAnimator animator = ObjectAnimator.ofFloat(qiblaName, "x", 420f);
-        animator.setDuration(animationDuration);
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(animator);
-        animatorSet.start();
-    }
-    */
 
+        // Link java object to defined XML button
+        initiate_button = findViewById(R.id.app_init_button);
+
+        initiate_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Initiate next activity to choose automatic or manual setup of app.
+                Intent intent = new Intent(MainActivity.this, ManOrAutoSetup.class);
+                startActivity(intent);
+            }
+        });
+
+    }
 
 }
---
+++ app/src/main/java/com/example/qibla/ManOrAutoSetup.java
@@ -0,0 +1,13 @@
+package com.example.qibla;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManOrAutoSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_or_auto_setup);
+    }
+}
